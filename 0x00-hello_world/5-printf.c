f997f43a31560038e42e1bd8e0fb08145acf4379
Gilbert Adikankwu <gilbert945@outlook.com>
2022-10-17 06:34:19 -0700

    feat: C program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
+ * main - entry point of the program
  *
- * Return - 0 for succeful execution
+ * Return:0 for succeful execution
  */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
