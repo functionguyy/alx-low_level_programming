77485a43bdcd681b43055870a34cdc84940ccf54
Gilbert Adikankwu <gilbert945@outlook.com>
2022-10-17 08:20:07 -0700

    feat: C program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 
 	printf("Size of a char: %i byte(s)\n", sizeof(a));
 	printf("Size of an int: %i byte(s)\n", sizeof(b));
-	printf("Size of a long long int: %i byte(s)\n", sizeof(c));
-	printf("Size of a float: %i byte(s)\n", sizeof(d));
+	printf("Size of a long int: %i byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(d));
 	printf("Size of a float: %i byte(s)\n", sizeof(e));
 	return (0);
 }
