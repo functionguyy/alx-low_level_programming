07e4742afd0abf003df833bb40d258824aebd4f4
Gilbert Adikankwu <gilbert945@outlook.com>
2022-10-17 06:20:02 -0700

    feat: A C program
    Write a C program that prints exactly write proper grammer, but the
    outcome is a piece of art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return - 0 for succeful execution
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return(0);
+}
