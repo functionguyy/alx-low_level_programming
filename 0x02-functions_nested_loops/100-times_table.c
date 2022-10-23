d572beafd667800688acf09d1ef9d527668bc668
Gilbert Adikankwu <gilbert945@outlook.com>
2022-10-23 01:36:59 -0700

    feat: printthe n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,26 +17,23 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				}
-				else if (k < 10 && j != 0)
+
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-				}
-				else if (k >= 10 && k < 100)
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
+				} else if (k >= 100)
 				{
-
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
@@ -46,7 +43,6 @@ void print_times_table(int n)
 			}
 			_putchar('\n');
 		}
-		
 	}
 }
 
