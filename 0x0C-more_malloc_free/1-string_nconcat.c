a8382b86b5678e4210001231751e6f39d20ded31
10xDatabro <gilbert945@outlook.com>
2022-11-22 22:26:56 +0100

    Add function that concatenates two strings
    Function concatenates two strings by adding the most n characters of the second
    string to the first string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,7 +17,7 @@ int _strlen(char *c)
 }
 
 /**
- * str_nconcat - concatenates two strings. If NULL is passed as argument of any
+ * string_nconcat - concatenates two strings. If NULL is passed as argument of any
  * of the string pointer parameter, the NULL value is treated as an
  * empty string
  * @s1: pointer to the first string literal
