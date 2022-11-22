63e7808ad1c0adcdced7e43942abfc0a7d46a668
10xDatabro <gilbert945@outlook.com>
2022-11-22 16:39:12 +0100

    Add function that free allocated memory
    Function frees the memory allocated for any 2-D array grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
-/**
+/*
 * free_grid - frees a 2-dimensional grid
-* @grid: pointer to allocated memory for 2-D array  
+* @grid: pointer to allocated memory for 2-D array
 * @height: Number of rows in the 2-D array
-*
+
 */
 
 void free_grid(int **grid, int height)
@@ -11,7 +11,7 @@ void free_grid(int **grid, int height)
 	int i;
 
 	i = 0;
-	while ( i < height)
+	while (i < height)
 	{
 		free(grid[i]);
 		i++;
