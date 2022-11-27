9cc6c1409bc2bc3043ba86001c6a7bff3b08c8e8
10xDatabro <gilbert945@outlook.com>
2022-11-27 08:33:45 +0100

    Add function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,16 +12,26 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0;
+	int idx = 0, i = 0;
 
-	while (index < n && src[index] != '\0')
+	while (src[idx] != '\0')
 	{
-		dest[index] = src[index];
-		index++;
+		idx++;
 	}
-	if (index < n)
+
+	while (i <= n)
 	{
-		dest[index] = '\0';
+		if ((src[i] == '\0') && (idx < n))
+		{
+			dest[i] = '\0';
+			return (dest);
+		}
+		else
+		{
+			dest[i] = src[i];
+		}
+
+		i++;
 	}
 
 	return (dest);
