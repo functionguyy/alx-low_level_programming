9a132e5b2874f2492bac1ffe104bb39aee0bb14c
10xDatabro <gilbert945@outlook.com>
2022-12-16 12:21:10 +0100

    feat: Function that encodes a string using ROT13
    This algorthim solves the task 8 of 0x06. C - More pointers, arrays and
    strings project.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+* rot13 - function that encodes a string using ROT13
+* @str: pointer to the string to be encoded
+*
+* Description: ROT13 is a simple letter substitution cipher that
+* replaces a letter with the 13th letter after it in the alphabet.
+* Return: pointer to the resulting string
+*/
+char *rot13(char *str)
+{
+	int loop_idx;
+	int check_idx;
+	int found_idx;
+	char *rot13_in =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot13_out =
+		"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	loop_idx = 0;
+	while (str[loop_idx] != '\0')
+	{
+		found_idx = 0;
+		check_idx = 0;
+		while (rot13_in[check_idx] != '\0')
+		{
+			if (str[loop_idx] == rot13_in[check_idx] &&
+					found_idx == 0)
+			{
+				str[loop_idx] = rot13_out[check_idx];
+				found_idx = check_idx;
+			}
+			check_idx++;
+		}
+		loop_idx++;
+	}
+	return (str);
+
+}
