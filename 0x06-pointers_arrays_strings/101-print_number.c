16f7de191bbe86d6f91059f67c8d371099ddbf29
10xDatabro <gilbert945@outlook.com>
2022-12-22 03:16:59 +0100

    feat: Function that prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+* print_number - prints an integer to standard output
+* @n: integer to be printed
+*
+* Return: nothing
+*/
+void print_number(int n)
+{
+	int num_pad;
+
+	num_pad = 10000000;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	if (n != 0)
+	{
+		while (n / num_pad == 0)
+		{
+			num_pad = num_pad / 10;
+		}
+		while (num_pad >= 1)
+		{
+			_putchar((n / num_pad) + '0');
+			n = n % num_pad;
+			num_pad = num_pad / 10;
+		}
+
+	}
+	else
+		_putchar('0');
+
+}
