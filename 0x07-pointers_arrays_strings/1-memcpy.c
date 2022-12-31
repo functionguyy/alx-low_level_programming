656c5bbd782b73afed872d7273d88af0c995b309
10xDatabro <gilbert945@outlook.com>
2022-12-31 09:28:38 +0100

    feat: A function that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+* _memcpy - function that copies memory area
+* @dest: destination string
+* @src: source string
+*
+* Return: pointer to destination string
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int loop_idx;
+
+	for (loop_idx = 0; loop_idx < n; loop_idx++)
+	{
+		*(dest + loop_idx) = *(src + loop_idx);
+	}
+	return (dest);
+
+}
