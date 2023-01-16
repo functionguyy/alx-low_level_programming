753e048973a3ff1f8dc14a985a0ce7d6543371e7
10xDatabro <gilbert945@outlook.com>
2023-01-16 22:36:36 +0100

    fix: Suppress unused attribute warning

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
 * main - prints the program name argument, followed by a new line
-* argc - count of command line argument passed to the program
-* argv - array of command line argument string
+* @argc: count of command line argument passed to the program
+* @argv: array of command line argument string
+*
+* Return: Always 0 for successful
 **/
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
