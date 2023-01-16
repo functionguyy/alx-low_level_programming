a8f1bd21471b070db52639d2232327fd246291c0
10xDatabro <gilbert945@outlook.com>
2023-01-16 22:08:12 +0100

    feat: A program that prints, its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/*
+/**
+* main - prints the program name argument, followed by a new line
+* argc - count of command line argument passed to the program
+* argv - array of command line argument string
 **/
 int main(int argc, char *argv[])
 {
