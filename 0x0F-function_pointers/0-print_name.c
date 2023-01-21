07a51791ab25e8e9290ffb878e872f0c17e214e7
10xDatabro <gilbert945@outlook.com>
2023-01-21 13:24:41 +0100

    feat: Add program that prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+/**
+* print_name - A function that prints a name to stdout
+* @name: pointer to the name to be printed
+* @f: function pointer to function that returns void and takes a character
+* pointer
+*
+*
+*/
+
+void print_name(char *name, void (*f)(char *s))
+{
+	(*f)(name);
+}
