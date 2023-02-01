bbe73975156da97fca861ec03cf1b60fdd8441a2
10xDatabro <gilbert945@outlook.com>
2023-02-01 22:14:12 +0100

    feat: Update header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 #include <stddef.h>
+#include <stdlib.h>
 /**
 * struct list_s - singly linked list
 * @str: string - (malloc'ed string)
@@ -17,4 +18,5 @@ typedef struct list_s
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
