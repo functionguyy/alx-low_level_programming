b8cda215e80a9d24e0ea5e62b46e351a9fe7d74c
10xDatabro <gilbert945@outlook.com>
2023-02-02 11:15:22 +0100

    feat: Add prototype to header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define _LISTS_H_
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 /**
 * struct list_s - singly linked list
 * @str: string - (malloc'ed string)
@@ -21,4 +22,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void print_before_main(void) __attribute__ ((constructor));
 #endif
