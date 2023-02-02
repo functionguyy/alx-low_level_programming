75e24089e1254d98713c436e574cad4730e40607
10xDatabro <gilbert945@outlook.com>
2023-02-02 11:14:50 +0100

    feat: Add function that excutes before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+* print_before_main - function executes before the main function is executed
+*
+* Return: Nothing
+*/
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
