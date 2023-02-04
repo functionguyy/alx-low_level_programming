1d05775e18ced273b3a9ec0e9b6ac5222a3c2eee
10xDatabro <gilbert945@outlook.com>
2023-02-04 16:40:02 +0100

    feat: Add function that prints all elements of a linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+/**
+* print_listint - function prints all the elements of a linked list.
+* @h: pointer to linked list
+*
+* Return: the numbers nodes in the linked list
+*/
+size_t print_listint(const listint_t *h)
+{
+	unsigned int n;
+
+	n = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
