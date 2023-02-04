57c1a75f53311f24a0f03b161e70de6806038682
10xDatabro <gilbert945@outlook.com>
2023-02-04 16:48:31 +0100

    feat: Add function that prints all the elements of a listint_t list.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,6 +5,7 @@
 * print_listint - function prints all the elements of a linked list.
 * @h: pointer to linked list
 *
+* Description: function prints all elements of a list of struct type listint_t
 * Return: the numbers nodes in the linked list
 */
 size_t print_listint(const listint_t *h)
