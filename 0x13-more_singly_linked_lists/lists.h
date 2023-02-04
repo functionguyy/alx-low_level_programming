de468ce9838a04b0b177c7125a0fdae54ba4f2e0
10xDatabro <gilbert945@outlook.com>
2023-02-04 16:53:43 +0100

    fix: Format style to match betty coding style

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,7 +4,7 @@
 * struct listint_s - singly linked list
 * @n: integer
 * @next: points to the next node
-* 
+*
 * Description: singly linked list node structure
 *
 */
@@ -12,6 +12,6 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
 size_t print_listint(const listint_t *h);
 #endif
