84eca3682a0b5f3f4933f20d34fc8bb3b6a2adc6
10xDatabro <gilbert945@outlook.com>
2023-02-16 14:08:10 +0100

    style: Reduce code lines to adhere to betty coding style
    Betty coding style limit function definition to 40 lines of code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,38 +18,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
 
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
-	{
 		return (0);
-	}
 
 	buf = malloc(sizeof(char) * letters);
 
 	if (buf == NULL)
-	{
 		return (0);
-	}
 
 	n_read = read(fd, buf, letters);
 
 	if (n_read == -1)
-	{
 		return (0);
-	}
 
 	n_write = write(STDOUT_FILENO, buf,  n_read);
 
 	if (n_write == -1 || n_write < n_read)
-	{
 		return (0);
-	}
-	
+
 	close(fd);
 
 	free(buf);
