f1d1f216a4f56444d44b2597dabf55d7c406b03f
Gilbert Adikankwu <gilbert945@outlook.com>
2023-03-17 16:28:32 +0100

    feat: Add function that returns the value of x rasied to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _pow_recursion - function that returns the value of x raised to the power of
+ * y
+ * @x: integer
+ * @y: power integer
+ *
+ * Description: if y is lower than 0, the function should return -1
+ * Return: value of x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, (y - 1)));
+}
