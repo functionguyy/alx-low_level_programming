76c0300e6d6be24e5ccaf19503fe07cfe3d0e6dd
Gilbert Adikankwu <gilbert945@outlook.com>
2023-03-17 08:24:44 +0100

    fix: Correct base condition

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,14 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-_putchar(*s);
-if (*s != '\0')
-{
-s++;
-_puts_recursion(s);
-}
-else
-{
-_putchar('\n');
-}
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
