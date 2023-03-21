401f4a160686ca239519644e4d3505e27984d7cf
Gilbert Adikankwu <gilbert945@outlook.com>
2023-03-21 19:04:47 +0100

    feat: Add function that deletes node at a given index
    To delete the node at index of a dlistint_t doubly linked list

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,111 @@
+#include "lists.h"
+#include  <stdlib.h>
+/**
+ * delete_dnodeint_at_index - function that deletes the node at index of a
+ * dlistint_t linked list
+ * @head: pointer to pointer to the beginning of the list
+ * @index: index of the node that should be deleted.
+ *
+ * Description: dlistint_t is a doubly linked list
+ * Return: 1 if it succeeded or -1 if it failed
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *cur = NULL;
+	size_t list_node_count;
+
+	if (head)
+	{
+		if (*head)
+		{
+			cur = *head;
+			/* delete first node in the list */
+			if (index == 0)
+			{
+				*head = cur->next;
+				cur->next->prev = cur->prev;
+			}
+			else
+			{
+				/* Get total length of the list */
+				list_node_count = dlistint_len(*head);
+
+				/* delete the last node in the list */
+				if (index == list_node_count - 1)
+				{
+					while (cur->next != NULL)
+						cur = cur->next;
+					cur->prev->next = cur->next;
+				}
+				else if (index < list_node_count - 1)
+				{
+					/* delete node in the middle of the list */
+					cur = get_dnodeint_at_index(*head, index);
+					cur->prev->next = cur->next;
+					cur->next->prev = cur->prev;
+				}
+			}
+			free(cur);
+			return (1);
+		}
+		return (-1);
+	}
+	return (-1);
+}
+
+/**
+ * dlistint_len - function that returns the number of element in a linked
+ * dlistint_t list
+ * @h: head pointer of dlistint_t list
+ *
+ * Return: the number of nodes in the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t node_count;
+
+	node_count = 0;
+
+	if (h)
+	{
+		while (h != NULL)
+		{
+			node_count++;
+			h = h->next;
+		}
+	}
+	return (node_count);
+}
+
+/**
+ * get_dnodeint_at_index - function that returns the nth node of a dlistint_t
+ * linked list
+ * @head: pointer to the beginning of the list
+ * @index: the index of the node, starting from 0
+ *
+ * Return: the address of the nth node or NULL if node does not exist
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count;
+	dlistint_t *cur_node;
+
+	count = 0;
+	cur_node = NULL;
+
+	if (head)
+	{
+		while (head != NULL)
+		{
+			cur_node = head;
+			head = cur_node->next;
+			if (count == index)
+				return (cur_node);
+			count++;
+		}
+	}
+	return (NULL);
+}
