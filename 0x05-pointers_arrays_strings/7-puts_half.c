b0284ad1ff3f949ba05a6cc42887e53f759cd58f
Gilbert Adikankwu <gilbert945@outlook.com>
2023-06-02 12:44:19 +0100

    fix: Change loop statements

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,29 +10,30 @@ void puts_half(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		;
+		i++;
 	}
 
 	if (i % 2 != 0)
 	{
 		i = (i + 1) / 2;
 
-		for (; str[i] != '\0';)
+		while (str[i] != '\0')
 		{
 			_putchar(str[i++]);
 		}
-		_putchar(10);
+		_putchar('\n');
 	}
 	else if (i % 2 == 0)
 	{
-		i /= 2;
+		i = i / 2;
 
-		for (; str[i] != '\0';)
+		while (str[i] != '\0')
 		{
 			_putchar(str[i++]);
 		}
-		_putchar(10);
+		_putchar('\n');
 	}
 }
