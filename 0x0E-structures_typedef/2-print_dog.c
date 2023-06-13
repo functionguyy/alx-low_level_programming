97bc974ade297f304069eeeb2b479170ef96fa3c
Gilbert Adikankwu <gilbert945@outlook.com>
2023-06-14 00:57:58 +0100

    fix: Change order of header file input

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,6 @@
+#include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "dog.h"
 /**
 * print_dog - prints the member values of a variable of type struct dog
 * @d: pointer to the struct dog variable
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		(d->name) ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
-		(d->age) ? printf("Age: %.f\n", d->age) : printf("Age: (nil)\n");
+		(d->age) ? printf("Age: %.6f\n", d->age) : printf("Age: (nil)\n");
 		(d->owner) ? printf("Owner: %s\n", d->owner) : printf("Owner: (nil)\n");
 
 	}
