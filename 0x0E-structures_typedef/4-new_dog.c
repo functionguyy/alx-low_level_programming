f026108ea39430653daf4d4295a65c956c677ecf
Gilbert Adikankwu <gilbert945@outlook.com>
2023-06-15 20:52:59 +0100

    fix: Store the name and owner parameter

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,33 +14,38 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *mem_alloc;
 	char *dog_name, *dog_owner;
+	char *dest_name, *dest_owner;
+	int len_owner, len_name;
 
 
 
 	mem_alloc = malloc(sizeof(dog_t));
 	if (mem_alloc == NULL)
-	{
 		return (NULL);
-	}
+	len_name = strlen(name) + 1;
+	len_owner = strlen(owner) + 1;
 
-	dog_name = strdup(name);
-	if (dog_name == NULL)
+	dest_name = malloc(sizeof(char) * len_name);
+	if (dest_name == NULL)
 	{
 		free(mem_alloc);
 		return (NULL);
 	}
 
-	dog_owner = strdup(owner);
-	if (dog_owner == NULL)
+	dest_owner = malloc(sizeof(char) * len_owner);
+	if (dest_owner == NULL)
 	{
-		free(dog_name);
+		free(dest_name);
 		free(mem_alloc);
 		return (NULL);
 	}
 
-	mem_alloc->name = dog_name;
+	dest_name = strcpy(name);
+	dest_owner = strcpy(owner);
+
+	mem_alloc->name = dest_name;
 	mem_alloc->age = age;
-	mem_alloc->owner = dog_owner;
+	mem_alloc->owner = dest_owner;
 
 
 	return (mem_alloc);
