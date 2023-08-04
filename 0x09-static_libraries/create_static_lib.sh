20eaaa6e6c3c258acd62bff5ee0f08f18b6f1d50
Gilbert Adikankwu <gilbert945@outlook.com>
2023-08-04 17:38:56 +0100

    docs: add comment

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# create object files .c files in current directory
 gcc -c -Wall -Werror -Wextra *.c
+# create static library named liball from .o files in current directory
 ar -rc liball.a *.o
+# index the liball.a static library
 ranlib liball.a
