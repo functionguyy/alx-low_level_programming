746da5e15a22cdadc10010c33c7ce36ffba0596a
Gilbert Adikankwu <gilbert945@outlook.com>
2023-08-04 10:51:04 +0100

    test: add directory containing test files

---
+++ 0x08-recursion/tests/100-main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+
+
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	int r;
+
+	r = is_palindrome("level");
+	printf("%d\n", r);
+	r = is_palindrome("redder");
+	printf("%d\n", r);
+	r = is_palindrome("test");
+	printf("%d\n", r);
+	r = is_palindrome("step on no pets");
+	printf("%d\n", r);
+	return (0);
+}
---
+++ 0x08-recursion/tests/3-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+
+int main(void)
+{
+	int r;
+
+	r = factorial(1);
+	printf("%d\n", r);
+	r = factorial(5);
+	printf("%d\n", r);
+	r = factorial(10);
+	printf("%d\n", r);
+	r = factorial(-1024);
+	printf("%d\n", r);
+	return (0);
+}
