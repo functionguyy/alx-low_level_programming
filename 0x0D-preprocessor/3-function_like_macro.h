383254e5c446db40fec5b4e96a6eaf1fe2bdeed9
Gilbert Adikankwu <gilbert945@outlook.com>
2023-08-14 22:30:12 +0100

    feat: add function-like macro ABS(x)

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,7 @@
 #ifndef _3_FUNCTION_LIKE_MACRO_H
 #define _3_FUNCTION_LIKE_MACRO_H
-#define ABS(X) ((X) < 0)? (-X):(X)
+#include <stdlib.h>
+
+#define ABS(x) abs(x)
 
 #endif /* _3_FUNCTION_LIKE_MACRO_H */
