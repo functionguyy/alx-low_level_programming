a7ef10dda1ba94a29ce7f5b5fc27ce8c7b656738
Gilbert Adikankwu <gilbert945@outlook.com>
2023-08-15 04:20:59 +0100

    fix: add include guard

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG
-#define DOG
+#ifndef DOG_H
+#define DOG_H
 /**
 * struct dog - dog structure
 * @name: First member
@@ -18,4 +18,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+#endif /* DOG_H *?
