19a131956e99ea3ba45f4157de4398c19cc2c8ab
Gilbert Adikankwu <gilbert945@outlook.com>
2023-08-25 02:33:09 +0100

    feat: add function that frees a linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * free_list - frees allocated memory in a linked list
+ * @head: pointer to the linked list
+ *
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	/* declare variables */
+	list_t *currentNode;
+
+
+	/* initialize variable */
+	currentNode = NULL;
+
+
+	if (head)
+	{
+		while (head != NULL)
+		{
+			currentNode = head;
+			head = head->next;
+			free(currentNode->str);
+			free(current)
+		}
+		free(head->str);
+		free(head);
+	}
+}
