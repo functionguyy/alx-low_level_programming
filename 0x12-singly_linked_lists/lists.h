ff341aca27a0e01e611f1b1e56dd10a3bc9fc80a
Gilbert Adikankwu <gilbert945@outlook.com>
2023-08-25 02:19:33 +0100

    feat: add prototype for function that adds a new node at the end of a linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -21,6 +22,6 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif /* LISTS_H */
