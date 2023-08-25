cd17473e0220ff7cc68041fc2eb10bbc117987b3
Gilbert Adikankwu <gilbert945@outlook.com>
2023-08-25 01:55:11 +0100

    feat: add a function that adds a new node at beginning of a linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: pointer to pointer to the last added node
+ * @str; pointer to str
+ *
+ * Return: the address of new element or NULL if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	/* declare variables */
+	list_t *newNode;
+
+
+	/* initialize variables */
+	newNode = NULL;
+
+	/* confirm list */
+	if (head == NULL)
+		return (NULL);
+
+
+	/* request memory allocation on heap */
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+		return (newNode);
+
+	if (str)
+	{
+		newNode->str = strdup(str);
+		newNode->len = strlen(str);
+	}
+
+	/* add new node to beginning of the list */
+	newNode->next = *head;
+	*head = newNode;
+
+	return (*head);
+}
