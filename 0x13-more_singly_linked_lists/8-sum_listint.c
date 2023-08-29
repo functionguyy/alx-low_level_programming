d2a9a3f3b18fc91f3b19079284d5f9fad74fd523
Gilbert Adikankwu <gilbert945@outlook.com>
2023-08-29 12:21:19 +0100

    feat: add function that returns the sum of alll data (n) of a singly linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * sum_listint - returns the sum of all the data (n) of a singly linked list
+ * @head: pointer to the singly linked list
+ *
+ * Return: the sum of all the data (n) or 0 if the list empty
+ *
+ */
+int sum_listint(listint_t *head)
+{
+	/* declare variables */
+	int sum;
+	listint_t *currentNode;
+
+
+	/* initialize variable */
+	sum = 0;
+	currentNode = NULL;
+
+
+	/* account for empty list */
+	if (head == NULL)
+		return (sum);
+
+
+	/* sum the data items in the list */
+	while (head != NULL)
+	{
+		currentNode = head;
+		sum += currentNode->n;
+		head = currentNode->n;
+	}
+
+	return (sum);
+}
