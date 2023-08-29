031b8c45210c402871cda02f762e36ddb81d19b6
Gilbert Adikankwu <gilbert945@outlook.com>
2023-08-29 11:29:38 +0100

    fix: remove node counter function and use while to handle comparison

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  * get_nodeint_at_index - returns the nth node of a singly linked list
  * @head: pointer to a singly linked list
@@ -10,55 +11,24 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	/* declare variables */
-	size_t count, ncount;
+	unsigned int count;
 	listint_t *nthNode;
 
 
 	/* initialize variables */
 	count = 0;
-	ncount = 0;
 	nthNode = NULL;
 
-	if (head == NULL)
-		return (nthNode);
-
-	/* count the nodes in the singly linked list */
-	ncount = listint_len(head);
 
 	/* seek the nth node */
-	while (count <= ncount)
+	while (head != NULL)
 	{
 		nthNode = head;
 		head = nthNode->next;
 		if (count == index)
-			break;
+			return(nthNode);
 		count++;
 	}
 
-	return (nthNode);
-}
-/**
- * listint_len - returns the number of elements in a singly linked list
- * @h: pointer to the singly linked list
- *
- * Description: returns the number of elements in a linked listint_t list
- * Return: returns the number of elements in a singly linked list
- */
-size_t listint_len(const listint_t *h)
-{
-	/* declare variables */
-	size_t n;
-
-	/* initialize variables */
-	n = 0;
-
-
-	/* count the nodes in the singly linked list */
-	while (h != NULL)
-	{
-		n++;
-		h = h->next;
-	}
-
-	return (n);
+	return (NULL);
 }
