13189ea1d88de195bcd6d16d2621c9531842fb60
Gilbert Adikankwu <gilbert945@outlook.com>
2023-09-04 23:02:40 +0100

    feat: add main header file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+
+
+/* helper function */
+
+#endif /* MAIN_H_ */
+
