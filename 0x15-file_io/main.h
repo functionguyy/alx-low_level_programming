8c979c035314b826cd48a554b9fa8a424a1b623a
Gilbert Adikankwu <gilbert945@outlook.com>
2023-09-05 04:27:55 +0100

    feat: add prototype of function that appends text at the end of a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,15 +5,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-
-
-
-
-
-/* helper function */
+int append _text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H_ */
 
