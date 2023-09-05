69a2d465378b7d2ce79ecc0c6b461f6370a77b55
Gilbert Adikankwu <gilbert945@outlook.com>
2023-09-05 05:43:29 +0100

    fix: update header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,10 +7,12 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
 
 #endif /* MAIN_H_ */
 
