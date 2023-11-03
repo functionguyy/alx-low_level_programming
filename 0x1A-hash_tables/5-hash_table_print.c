20ae4753b4c952786e099fe542fc134b9f8d9256
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-11-04 00:05:09 +0100

    feat: print hash_table_t type  hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_print - print a hash_table of data structure hash_table_t
+ * @ht: the hash table
+ *
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	/* declare variables */
+	unsigned long int size, idx;
+	hash_node_t *head, *cur_node;
+	int start;
+
+	idx = 0;
+	start = 1;
+	putchar('{');
+	if (ht)
+	{
+		size = ht->size;
+
+		for (idx = 0; idx < size; idx++)
+		{
+			/* get the element at current idx */
+			head = ht->array[idx];
+
+			if (head)
+			{
+				if (start)
+					start = 0;
+				else
+					putchar(',');
+
+				while (head != NULL)
+				{
+					cur_node = head;
+					head = cur_node->next;
+					printf("'%s': '%s'", cur_node->key, cur_node->value);
+
+				}
+
+			}
+		}
+
+	}
+	puts("}");
+}
