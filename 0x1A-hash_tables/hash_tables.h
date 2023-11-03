d1125e908eb8c11fb2026fa0a95749ff195a1bee
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-11-03 23:03:58 +0100

    fix: add semicolon at the end of struct hash_table_t

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -13,7 +13,7 @@ typedef struct hash_node_s
 	char *key;
 	char *value;
 	struct hash_node_s *next;
-} hash_node_t
+} hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
@@ -34,5 +34,6 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
 #endif /* HASH_TABLES_H */
 
