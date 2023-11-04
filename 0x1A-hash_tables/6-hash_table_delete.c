a5ba8f7dc1af7a6c124a908b3bb7e54be1d6d6e6
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-11-04 06:28:32 +0100

    feat: delete hash table type hash_table_t

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes a hash table of type hash_table_t
+ * @ht: the hash_table
+ *
+ * Return: Nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	/* declare variables */
+	hash_node_t *cur_node, *head;
+	unsigned long int idx;
+
+
+	idx = 0;
+
+	if (ht)
+	{
+		for (idx = 0; idx < ht->size; idx++)
+		{
+			head = ht->array[idx];
+
+			if (head)
+			{
+				while (head != NULL)
+				{
+					cur_node = head;
+					head = head->next;
+					free(cur_node->key);
+					free(cur_node->value);
+					free(cur_node);
+				}
+
+			}
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
