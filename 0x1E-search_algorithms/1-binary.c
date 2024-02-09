0433eab3426ed027b9415c7eea6150c4dedfc215
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-02-09 21:18:00 +0100

    feat: initial codes for binary_search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,22 +12,36 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t l, r, m;
+	size_t l, r, m, i;
 
 	l = 0;
 	r = size - 1;
 
 	while (l <= r)
 	{
-		m = (l + r) / 2;
+		m = l + (r - l) / 2;
+
+		printf("Searching in array: ");
+		for (i = l; i <= r;)
+		{
+			printf("%d", array[i]);
+
+			if (i != r)
+				printf(", ");
+			i++;
+		}
+		printf("\n");
+
 
 		if (array[m] < value)
 			l = m + 1;
 		else if (array[m] > value)
 			r = m - 1;
 		else
-			return m;
+			return (m);
 	}
+
+	return (-1)
 
 
 }
