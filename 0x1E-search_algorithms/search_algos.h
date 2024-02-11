1bb154dbf9eb85ffbf5d41770b63c9558de28751
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-02-11 19:51:01 +0100

    feat: add linear_skip data structure prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,4 +4,25 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ *
+ *
+ *
+ *
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+skiplist_t *linear_skip(skiplist_t *list, int value);
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
+
 #endif /* SEARCH_ALGOS_H */
