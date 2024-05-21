4e2cf3fc2f38d337548d7e7b152d367f911fa4cb
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-05-21 13:27:47 +0100

    feat: linear search algorithm use case
    function that searches for a value in an array of integers using the
    linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "search_algos.h"
+/**
+ * linear_search - searches for a value in an array of integers using linear
+ * search algorithm
+ * @array: pointer to the first element of an array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: first index where value is located, if value is not present in array
+ * or if array is NULL return -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t idx;
+
+	idx = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	while (idx < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
+		if (array[idx] == value)
+			return (idx);
+		idx++;
+	}
+	return (-1);
+}
