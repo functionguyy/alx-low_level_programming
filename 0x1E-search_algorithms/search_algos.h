22bf7da4ad80dd3e38494143a8065e5b90bbb3da
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-07-20 02:24:33 +0100

    feat: search for value in array of integers

--- 0x1E-search_algorithms/search_algos.h
+++
@@ -1,7 +0,0 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
-
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-
-#endif /* SEARCH_ALGOS_H */
